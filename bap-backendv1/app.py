c2dc23997376f6d1cb2c39436923500ff7cd80b5
Bisaam Hassan <bisaam221@gmail.com>
2023-10-15 22:50:48 -0500

    added login api functionality

--- bap-backendv1/app.py
+++ bap-backendv1/app.py
@@ -1,13 +1,58 @@
 from flask import Flask, render_template, request, jsonify 
-from pymango import MongoClient
+from pymongo import MongoClient
 from bson import ObjectId
+from flask_cors import CORS
 import os
+from config import config
 
 app = Flask(__name__)
 
-uri = os.environ["MONGO_URI"]
+uri = config["MONGO_URI"]
 client = MongoClient(uri)
-db = client[]
+db = client["BAP-MAIN"]
+
+CORS(app)
+
+@app.route('/')
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
+
+    user = request.args.get('user')
+    password = request.args.get('pw')
+
+    if(user and password):
+        if request.method == 'GET':
+            foundUser = db['UserAuth'].find_one({'username':user})
+            if foundUser and foundUser['password'] == password:
+                return jsonify({
+                    "status": 'Successfully Logged in (valid user and pass)'
+                })
+            else:
+                return jsonify({
+                    "status":"Failed to login (username not found or pass incorrect)"
+                })
+        # elif request.method == 'POST':
+        #     db['UserAuth'].insert_one({
+        #         'username':user,
+        #         'password':password
+        #     })
+        #     return jsonify({
+        #         'status':'Successfully created an account'
+        #     })
+    else:
+        return jsonify({
+            'status': 'Didnt fill in either user or pass'
+        })
+        
+            
+        
+
+        
+    
+    
+
+
+
 
 
 
--- bap-backendv1/config/__init__.py
+++ bap-backendv1/config/__init__.py
@@ -5,8 +5,8 @@ load_dotenv()
 uri = 'mongodb+srv://bisaamh:bisaamhassan@bap-hrd.wlvquy6.mongodb.net/?retryWrites=true&w=majority'
 
 
-
+config = os.environ
 DB_USERNAME = os.environ["DB_USERNAME"]
 DB_PASS = os.environ["DB_PASS"]
 DB_HOST = os.environ["DB_HOST"]
-os.environ["MONGO_URI"] = f"mongodb+srv://{DB_USERNAME}:{DB_PASS}@{DB_HOST}"
+config["MONGO_URI"] = f"mongodb+srv://{DB_USERNAME}:{DB_PASS}@{DB_HOST}"
