359f1557c09a68522ba50306e5107e8df43f53a9
Bisaam Hassan <bisaam221@gmail.com>
2023-11-29 23:54:45 -0600

    loginservice.js changes

--- bap-frontendv1/src/services/LoginService.js
+++ bap-frontendv1/src/services/LoginService.js
@@ -2,8 +2,8 @@ import axios from 'axios'
 
 const LOGIN_API_URL = 'http://127.0.0.1:5000/login'
 
-function attemptLogin(attemptUser, attemptPass){
-    return axios.get(LOGIN_API_URL, {params: {user: attemptUser, pw: attemptPass}})
+function attemptLogin(attemptUser, attemptPass, currUser){
+    return axios.get(LOGIN_API_URL, {params: {user: attemptUser, pw: attemptPass, currentUser: currUser}})
 }
 
 
